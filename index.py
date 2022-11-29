882d1de6288efd371aa2d7fc5b43eeba51d23b10
김경한 <2015004002@hanyang.ac.kr>
2022-11-30 03:08:31 +0900

    use .env

--- index.py
+++ index.py
@@ -1,12 +1,15 @@
+import os
 from SlackAPI import SlackAPI
-from os import environ
 
-slack = SlackAPI(environ['OAUTH_TOKEN'])
+token = os.getenv('OAUTH_TOKEN')
 
-channel_name = "bot-alarm"
-query = "!"
-text = "test"
+if token is not None:
+  slack = SlackAPI(token)
 
-channel_id = slack.get_channel_id(channel_name)
-message_ts = slack.get_message_ts(channel_id, query)
-slack.post_thread_message(channel_id, message_ts, text)
+  channel_name = "bot-alarm"
+  query = "!"
+  text = "test"
+
+  channel_id = slack.get_channel_id(channel_name)
+  message_ts = slack.get_message_ts(channel_id, query)
+  slack.post_thread_message(channel_id, message_ts, text)
---
+++ util.py
@@ -0,0 +1,2 @@
+def get_text(query, time, url):
+    return query + ' ' + time + ' 예약\n주소 : ' + url
